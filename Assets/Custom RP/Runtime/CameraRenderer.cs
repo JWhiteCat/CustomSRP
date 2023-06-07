b49beebfc75dcf7b4dc3fdca880819d851df64a0
MAIN\wanj <wanjian.wan@alibaba-inc.com>
2023-06-07 23:27:11 +0800

    Custom Render Pipeline 绘制物体、线框、UI

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareForSceneWindow();
+
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+    }
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    const string bufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+        context.SetupCameraProperties(camera);
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    CullingResults cullingResults;
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+}
